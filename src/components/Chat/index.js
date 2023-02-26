ea42eb0c53789ef1f355832104ee61e9308622c3
Dainwi <danwichoudhary@gmail.com>
2023-02-26 23:53:40 +0530

    Chat list item component is completed

---
+++ src/components/Chat/index.js
@@ -0,0 +1,59 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const ChatListItem = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text style={styles.name} numberOfLines={1}>
+            Lukas
+          </Text>
+          <Text style={styles.subTitle}>23:38</Text>
+        </View>
+
+        <Text style={styles.subTitle} numberOfLines={1}>
+          Hello there
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#ddd",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    color: "gray",
+  },
+});
+
+export default ChatListItem;
