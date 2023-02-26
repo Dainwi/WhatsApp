8e3ee836ebf1a48d30a2576c251b2712ac0d1d75
Dainwi <danwichoudhary@gmail.com>
2023-02-27 01:36:21 +0530

    input box is created

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,18 +1,30 @@
-import { View, FlatList, ImageBackground, StyleSheet } from "react-native";
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  KeyboardAvoidingView,
+} from "react-native";
 import Message from "../components/Messages";
 import bg from "../../assets/images/BG.png";
 import messages from "../../assets/data/messages.json";
+import InputBox from "../components/InputBox";
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={StyleSheet.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        style={styles.list}
-        inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={StyleSheet.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
